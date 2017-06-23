5093fe9a914aeb3d67921949699779a56e0848c8
jackie <liuzj_jj@163.com>
2017-06-23 15:35:28 +0800

    电子发票

---
+++ common/src/main/java/com/yumu/hexie/integration/wuye/vo/InvoiceInfo.java
@@ -0,0 +1,42 @@
+package com.yumu.hexie.integration.wuye.vo;
+
+public class InvoiceInfo {
+
+	private String mobile;
+	private String invoice_title_type;
+	private String invoice_title;
+	private String credit_code;
+	private String showcom;
+	
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getInvoice_title_type() {
+		return invoice_title_type;
+	}
+	public void setInvoice_title_type(String invoice_title_type) {
+		this.invoice_title_type = invoice_title_type;
+	}
+	public String getInvoice_title() {
+		return invoice_title;
+	}
+	public void setInvoice_title(String invoice_title) {
+		this.invoice_title = invoice_title;
+	}
+	public String getCredit_code() {
+		return credit_code;
+	}
+	public void setCredit_code(String credit_code) {
+		this.credit_code = credit_code;
+	}
+	public String getShowcom() {
+		return showcom;
+	}
+	public void setShowcom(String showcom) {
+		this.showcom = showcom;
+	}
+	
+}
