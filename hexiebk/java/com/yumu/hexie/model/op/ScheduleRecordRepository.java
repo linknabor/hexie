f3f4d209cd4d827d16ca096083bce07243066338
davidhardson <davidhardson@hotmail.com>
2018-10-23 17:57:16 +0800

    remove unused files

--- hexiebk/java/com/yumu/hexie/model/op/ScheduleRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.yumu.hexie.model.op;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRecordRepository extends JpaRepository<ScheduleRecord, Long> {
-}
