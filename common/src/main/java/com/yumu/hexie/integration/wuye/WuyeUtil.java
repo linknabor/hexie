75e5d7d0b788780c54d2ffa52d4cecd99abf3e89
wangyw <532858859@qq.com>
2019-03-15 15:02:04 +0800

    测试机访问地址为生产  改为测试

--- common/src/main/java/com/yumu/hexie/integration/wuye/WuyeUtil.java
+++ common/src/main/java/com/yumu/hexie/integration/wuye/WuyeUtil.java
@@ -35,7 +35,7 @@ import com.yumu.hexie.integration.wuye.vo.WechatPayInfo;
 public class WuyeUtil {
 	private static final Logger log = LoggerFactory.getLogger(WuyeUtil.class);
 
-	private static String REQUEST_ADDRESS = "http://www.e-shequ.com/mobileInterface/mobile/";
+	private static String REQUEST_ADDRESS = "http://test.e-shequ.com/mobileInterface/mobile/";
 	private static String SYSTEM_NAME;
 	private static Properties props = new Properties();
 	
