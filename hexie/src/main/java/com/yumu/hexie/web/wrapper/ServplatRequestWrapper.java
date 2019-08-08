3e560873123a076359702a594df34e7e129ae90e
davidhardson <davidhardson@hotmail.com>
2019-08-08 11:21:05 +0800

    过滤器重写流指定字符集

--- hexie/src/main/java/com/yumu/hexie/web/wrapper/ServplatRequestWrapper.java
+++ hexie/src/main/java/com/yumu/hexie/web/wrapper/ServplatRequestWrapper.java
@@ -71,7 +71,7 @@ public class ServplatRequestWrapper extends HttpServletRequestWrapper {
 	@Override
 	public ServletInputStream getInputStream() throws IOException {
 		
-		final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
+		final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes("UTF-8"));
 		ServletInputStream servletInputStream = new ServletInputStream() {
 			public boolean isFinished() {
 				return false;
