6aa476a60efe71848faca75f6681f8515796fb4d
davidhardson <davidhardson@hotmail.com>
2019-12-16 15:06:14 +0800

    修复用户注册后，异步更新wuyeId，将手机号覆盖成null的bug

--- common/src/main/java/com/yumu/hexie/service/user/UserService.java
+++ common/src/main/java/com/yumu/hexie/service/user/UserService.java
@@ -41,7 +41,7 @@ public interface UserService {
 	public List<User> getUserByShareCode(String shareCode);
 	User updateUserLoginInfo(UserWeiXin weixinUser, String oriApp);
 	User multiFindByOpenId(String openId);
-	User bindWuYeId(User user);
+	void bindWuYeId(User user);
 	boolean checkDuplicateLogin(HttpSession httpSession);
 
 
