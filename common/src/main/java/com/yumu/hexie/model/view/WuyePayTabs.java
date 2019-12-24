b726946a508821fd7c49394baf4a62adb5c2346a
davidhardson <davidhardson@hotmail.com>
2019-12-24 13:16:38 +0800

    物业缴费选项卡配置

--- common/src/main/java/com/yumu/hexie/model/view/WuyePayTabs.java
+++ common/src/main/java/com/yumu/hexie/model/view/WuyePayTabs.java
@@ -13,6 +13,7 @@ public class WuyePayTabs extends BaseModel {
 	private static final long serialVersionUID = 7134137096593878685L;
 	
 	private String name;	//选项卡名称
+	private String value;	//选项卡值
 	private String sort;	//选项卡顺序
 	private String appId;	//公众号APPID
 	
@@ -34,10 +35,15 @@ public class WuyePayTabs extends BaseModel {
 	public void setAppId(String appId) {
 		this.appId = appId;
 	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
 	@Override
 	public String toString() {
-		return "WuyePayTabs [name=" + name + ", sort=" + sort + ", appId=" + appId + "]";
+		return "WuyePayTabs [name=" + name + ", value=" + value + ", sort=" + sort + ", appId=" + appId + "]";
 	}
 	
-
 }
