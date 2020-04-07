c7bbc36140c17f979ab0aff2e8bfa06b355554b3
davidhardson <davidhardson@hotmail.com>
2020-04-07 17:43:17 +0800

    消息推送

--- common/src/main/java/com/yumu/hexie/service/hexiemessage/HexieMessageService.java
+++ common/src/main/java/com/yumu/hexie/service/hexiemessage/HexieMessageService.java
@@ -5,7 +5,8 @@ import java.util.List;
 import com.yumu.hexie.model.hexiemessage.HexieMessage;
 
 public interface HexieMessageService {
+	
 	void pullWechat(HexieMessage exr);
 	
-	public List<HexieMessage> getMessage(long userId);
+	List<HexieMessage> getMessage(long userId);
 }
--- hexie/src/main/java/com/yumu/hexie/web/hexiemessage/HexieMessageController.java
+++ hexie/src/main/java/com/yumu/hexie/web/hexiemessage/HexieMessageController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yumu.hexie.model.community.Message;
 import com.yumu.hexie.model.hexiemessage.HexieMessage;
 import com.yumu.hexie.service.hexiemessage.HexieMessageService;
 import com.yumu.hexie.web.BaseController;
