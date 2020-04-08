8f738f2473d1c2d18f5bf5899d3598f58f297949
eshequ <39544487+eshequ@users.noreply.github.com>
2020-04-08 13:41:45 +0800

    fix uat (#94)
    Co-authored-by: davidhardson <davidhardson@hotmail.com>

--- common/src/main/java/com/yumu/hexie/service/hexiemessage/HexieMessageService.java
+++ common/src/main/java/com/yumu/hexie/service/hexiemessage/HexieMessageService.java
@@ -1,11 +1,8 @@
 package com.yumu.hexie.service.hexiemessage;
 
-import java.util.List;
-
 import com.yumu.hexie.model.hexiemessage.HexieMessage;
 
 public interface HexieMessageService {
-	void pullWechat(HexieMessage exr);
 	
 	void sendMessage(HexieMessage exr);
 	
