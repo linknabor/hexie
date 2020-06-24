d8abf3a63591826263b04fd3b8b9dc8184c99dea
davidhardson <davidhardson@hotmail.com>
2020-06-24 18:24:50 +0800

    调整SQL

--- common/src/main/java/com/yumu/hexie/model/localservice/ServiceOperatorRepository.java
+++ common/src/main/java/com/yumu/hexie/model/localservice/ServiceOperatorRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 
@@ -77,6 +78,7 @@ public interface ServiceOperatorRepository  extends JpaRepository<ServiceOperato
     
     public ServiceOperator findByTypeAndTelAndOpenId(int type, String tel, String openId);
     
+    @Modifying
     @Query(nativeQuery = true, value = "delete from serviceoperator where type = ?1 ")
     public void deleteByType(int type);
 
