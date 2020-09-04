d5f72842a6ccc2e43983f1eaad6603369947695d
eshequ <39544487+eshequ@users.noreply.github.com>
2020-09-04 17:13:49 +0800

    Devhuym29 (#151)
    * 重置密码增加短信验证码

--- hexie/src/main/java/com/yumu/hexie/web/eshop/PromotionController.java
+++ hexie/src/main/java/com/yumu/hexie/web/eshop/PromotionController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yumu.hexie.common.Constants;
@@ -22,16 +23,18 @@ import com.yumu.hexie.web.BaseResult;
 @RestController
 @RequestMapping(value = "/promotion")
 public class PromotionController extends BaseController {
-
+	
 	@Autowired
 	private PromotionService promotionService;
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/password/reset", method = RequestMethod.GET)
-	public BaseResult<String> resetPassword(@ModelAttribute(Constants.USER)User user) throws Exception{
+	public BaseResult<String> resetPassword(@ModelAttribute(Constants.USER)User user, @RequestParam String vericode) throws Exception{
 		
-		promotionService.resetPassword(user);
+		promotionService.resetPassword(user, vericode);
 		return BaseResult.successResult(Constants.PAGE_SUCCESS);
 		
 	}
+	
+	
 }
