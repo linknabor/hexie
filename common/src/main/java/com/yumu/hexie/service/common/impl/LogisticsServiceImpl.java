53121ccefe8fb21fa4a9bf1c10559cecaaba947c
davidhardson <davidhardson@hotmail.com>
2020-09-17 16:16:53 +0800

    fix logistics status

--- common/src/main/java/com/yumu/hexie/service/common/impl/LogisticsServiceImpl.java
+++ common/src/main/java/com/yumu/hexie/service/common/impl/LogisticsServiceImpl.java
@@ -119,7 +119,7 @@ public class LogisticsServiceImpl implements LogisticsService {
 	public void saveLogisticsInfo(LogisticsInfoReq logisticsInfoReq) {
 
 		ServiceOrder serviceOrder = serviceOrderRepository.findById(logisticsInfoReq.getOrderId()).get();
-		if (ModelConstant.ORDER_STATUS_PAYED != serviceOrder.getStatus()) {
+		if (ModelConstant.ORDER_STATUS_CONFIRM != serviceOrder.getStatus()) {
 			throw new BizValidateException("订单状态不允许当前操作，订单ID：" + serviceOrder.getId());
 		}
 		serviceOrder.setLogisticCode(logisticsInfoReq.getLogisticCode());
