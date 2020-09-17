693fa646dfe6450fb8a84f138639ad4fc15afedf
davidhardson <davidhardson@hotmail.com>
2020-09-17 16:42:20 +0800

    fix findBystatus

--- hexie/src/main/java/com/yumu/hexie/web/sales/OrderController.java
+++ hexie/src/main/java/com/yumu/hexie/web/sales/OrderController.java
@@ -98,6 +98,8 @@ public class OrderController extends BaseController{
 			status.add(ModelConstant.ORDER_STATUS_PAYED);
 		}else if("PREPARE".equalsIgnoreCase(statusType)){
 			status.add(ModelConstant.ORDER_STATUS_CONFIRM);
+		}else if("SENT".equalsIgnoreCase(statusType)){
+			status.add(ModelConstant.ORDER_STATUS_SENDED);	
 		}else{//if("ALL".equalsIgnoreCase(statusType)){
 			status.add(ModelConstant.ORDER_STATUS_INIT);
 			status.add(ModelConstant.ORDER_STATUS_PAYED);
