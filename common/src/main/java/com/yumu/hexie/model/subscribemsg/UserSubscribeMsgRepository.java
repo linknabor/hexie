38c4ee5f0fca60f8efbe0be9edc27f3c0360e509
davidhardson <davidhardson@hotmail.com>
2021-02-24 15:08:45 +0800

    remove unused codes

--- common/src/main/java/com/yumu/hexie/model/subscribemsg/UserSubscribeMsgRepository.java
+++ common/src/main/java/com/yumu/hexie/model/subscribemsg/UserSubscribeMsgRepository.java
@@ -1,12 +1,8 @@
 package com.yumu.hexie.model.subscribemsg;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserSubscribeMsgRepository extends JpaRepository<UserSubscribeMsg, Long> {
 
-	List<UserSubscribeMsg> findByOpenidAndBizType(String openid, int bizType);
-	
 	UserSubscribeMsg findByOpenidAndTemplateId(String openid, String templateId);
 }
