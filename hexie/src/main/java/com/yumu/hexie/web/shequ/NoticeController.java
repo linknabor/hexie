fc2bcd81ea0bead91981d19bc4c9832cff118789
liuzj_jj <liuzj_jj@163.com>
2021-06-29 15:08:30 +0800

    社区圈

--- hexie/src/main/java/com/yumu/hexie/web/shequ/NoticeController.java
+++ hexie/src/main/java/com/yumu/hexie/web/shequ/NoticeController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,7 @@ import com.yumu.hexie.web.shequ.vo.NoticeVO;
 @RequestMapping("/notice")
 public class NoticeController extends BaseController {
 
+	private static final Logger log = LoggerFactory.getLogger(NoticeController.class);
 	@Autowired
 	private NoticeService noticeService;
 	
@@ -37,7 +40,8 @@ public class NoticeController extends BaseController {
 	}
 
 	@RequestMapping(value = "/addOutSidNotice", produces = "text/html;charset=UTF-8", method = RequestMethod.POST)
-	public BaseResult<String> addOutSidNotice(@RequestParam Map<String, String> params) {
+	public BaseResult<String> addOutSidNotice(@RequestBody Map<String, String> params) {
+		log.debug("params :" + params);
 		String id = noticeService.addOutSidNotice(params);
 		return BaseResult.successResult(id);
 	}
