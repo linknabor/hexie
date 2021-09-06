77d89ae864bb491ddc07aa453a3a355c9d33988f
liuzj_jj <liuzj_jj@163.com>
2021-09-06 16:29:16 +0800

    停车场

--- common/src/main/java/com/yumu/hexie/integration/park/resp/PayDetail.java
+++ common/src/main/java/com/yumu/hexie/integration/park/resp/PayDetail.java
@@ -9,11 +9,13 @@ package com.yumu.hexie.integration.park.resp;
 public class PayDetail {
     private String order_id;
     private String car_no;
+    private String park_name;
     private String in_park_time;
     private String out_park_time;
     private String park_time;
     private String tran_date;
     private String acct_date;
+    private String tran_amt;
 
     public String getOrder_id() {
         return order_id;
@@ -71,16 +73,34 @@ public class PayDetail {
         this.acct_date = acct_date;
     }
 
+    public String getPark_name() {
+        return park_name;
+    }
+
+    public void setPark_name(String park_name) {
+        this.park_name = park_name;
+    }
+
+    public String getTran_amt() {
+        return tran_amt;
+    }
+
+    public void setTran_amt(String tran_amt) {
+        this.tran_amt = tran_amt;
+    }
+
     @Override
     public String toString() {
         return "PayDetail{" +
                 "order_id='" + order_id + '\'' +
                 ", car_no='" + car_no + '\'' +
+                ", park_name='" + park_name + '\'' +
                 ", in_park_time='" + in_park_time + '\'' +
                 ", out_park_time='" + out_park_time + '\'' +
                 ", park_time='" + park_time + '\'' +
                 ", tran_date='" + tran_date + '\'' +
                 ", acct_date='" + acct_date + '\'' +
+                ", tran_amt='" + tran_amt + '\'' +
                 '}';
     }
 }
