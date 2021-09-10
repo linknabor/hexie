195cfe8984b43ae98701fe2739f9392585df5110
liuzj_jj <liuzj_jj@163.com>
2021-09-10 15:01:17 +0800

    停车场

--- common/src/main/java/com/yumu/hexie/integration/park/resp/ParkInfo.java
+++ common/src/main/java/com/yumu/hexie/integration/park/resp/ParkInfo.java
@@ -12,6 +12,9 @@ public class ParkInfo {
     private String park_id;
     private String park_name;
     private String park_addr;
+    private String park_tel;
+    private String cust_prompt;
+    private String out_park_prompt;
     private List<RuleInfo> ruleList;
 
     public static class RuleInfo {
@@ -55,6 +58,30 @@ public class ParkInfo {
 
     public void setPark_addr(String park_addr) {
         this.park_addr = park_addr;
+    }
+
+    public String getPark_tel() {
+        return park_tel;
+    }
+
+    public void setPark_tel(String park_tel) {
+        this.park_tel = park_tel;
+    }
+
+    public String getCust_prompt() {
+        return cust_prompt;
+    }
+
+    public void setCust_prompt(String cust_prompt) {
+        this.cust_prompt = cust_prompt;
+    }
+
+    public String getOut_park_prompt() {
+        return out_park_prompt;
+    }
+
+    public void setOut_park_prompt(String out_park_prompt) {
+        this.out_park_prompt = out_park_prompt;
     }
 
     public List<RuleInfo> getRuleList() {
