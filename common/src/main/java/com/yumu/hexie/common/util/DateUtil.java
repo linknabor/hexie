5ea4444ddf2520c2152a33dfec5b3c31c4f272cf
liuzj_jj@163.com <liuzj_jj@163.com>
2021-11-10 14:54:43 +0800

    业主意见统计接口

--- common/src/main/java/com/yumu/hexie/common/util/DateUtil.java
+++ common/src/main/java/com/yumu/hexie/common/util/DateUtil.java
@@ -4,25 +4,25 @@ import org.springframework.util.StringUtils;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-import org.springframework.util.StringUtils;
-
 public class DateUtil {
-    public static final String dSimple    = "yyyy-MM-dd";
+    public static final String dSimple = "yyyy-MM-dd";
     public static final String dttmSimple = "yyyy-MM-dd HH:mm:ss";
     public static final String dateSimple = "EEE MMM dd HH:mm:ss z yyyy";
 
     public static Date addDate(Date current, int addedDays) {
-    	 Calendar   calendar   =   new   GregorianCalendar(); 
-         calendar.setTime(current); 
-         calendar.add(Calendar.DATE,addedDays);//把日期往后增加一天.整数往后推,负数往前移动 
-         return calendar.getTime();  
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(current);
+        calendar.add(Calendar.DATE, addedDays);//把日期往后增加一天.整数往后推,负数往前移动
+        return calendar.getTime();
     }
+
     public static String getSendTime(long time) {
         Calendar cal = Calendar.getInstance();
         long l = cal.getTimeInMillis() - time;
@@ -54,10 +54,10 @@ public class DateUtil {
             return null;
         }
         try {
-	        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-	        if (dateSimple.equals(pattern)) {
-	        	sdf = new SimpleDateFormat(pattern, Locale.US);
-	        }
+            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+            if (dateSimple.equals(pattern)) {
+                sdf = new SimpleDateFormat(pattern, Locale.US);
+            }
             return sdf.parse(timeText);
         } catch (ParseException e) {
             throw new IllegalArgumentException(e);
@@ -66,6 +66,7 @@ public class DateUtil {
 
     /**
      * yyyy-MM-dd HH:mm
+     *
      * @param date
      * @return
      */
@@ -78,8 +79,9 @@ public class DateUtil {
     }
 
     public static final String dtFormat(long date, String format) {
-    	return dtFormat(new Date(date),format);
+        return dtFormat(new Date(date), format);
     }
+
     /**
      * @param date
      * @return
@@ -94,9 +96,8 @@ public class DateUtil {
 
     /**
      * yyyy-MM-dd
-     * 
+     *
      * @param date
-     * 
      * @return
      */
     public static final String dtFormat(Date date) {
@@ -108,75 +109,78 @@ public class DateUtil {
     }
 
     /**
-     *优先格式化成yyyy-MM-dd形式的Date，如果不传时分秒，则取当天零点
-     *description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
-     *@author rongnian.lu gfwu321@163.com
-     *date： 2015-7-28
-     *param:@param dateString
-     *param:@return
-     *return：Date
+     * 优先格式化成yyyy-MM-dd形式的Date，如果不传时分秒，则取当天零点
+     * description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
+     *
+     * @author rongnian.lu gfwu321@163.com
+     * date： 2015-7-28
+     * param:@param dateString
+     * param:@return
+     * return：Date
      */
     public static final Date getDateFromString(String dateString) {
-    	Date date=null;
-    	try {
-    		date=	new SimpleDateFormat(dSimple).parse(dateString);
-		} catch (Exception e) {
-			try {
-				date=	new SimpleDateFormat(dttmSimple).parse(dateString);
-			} catch (Exception e2) {
-				date=null;
-			}
-		}
-    	
+        Date date = null;
+        try {
+            date = new SimpleDateFormat(dSimple).parse(dateString);
+        } catch (Exception e) {
+            try {
+                date = new SimpleDateFormat(dttmSimple).parse(dateString);
+            } catch (Exception e2) {
+                date = null;
+            }
+        }
+
         return date;
     }
-    
+
     /**
-     *优先格式化成yyyy-MM-dd HH:mm:ss形式的Date，如果不传时分秒，则取当天零点 
-     *description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
-     *@author rongnian.lu gfwu321@163.com
-     *date： 2015-7-28
-     *param:@param dateString
-     *param:@return
-     *return：Date
+     * 优先格式化成yyyy-MM-dd HH:mm:ss形式的Date，如果不传时分秒，则取当天零点
+     * description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
+     *
+     * @author rongnian.lu gfwu321@163.com
+     * date： 2015-7-28
+     * param:@param dateString
+     * param:@return
+     * return：Date
      */
     public static final Date getDateTimeFromString(String dateString) {
-    	Date date=null;
-    	try {
-    		date=	new SimpleDateFormat(dttmSimple).parse(dateString);
-		} catch (Exception e) {
-			date=null;
-		}
+        Date date = null;
+        try {
+            date = new SimpleDateFormat(dttmSimple).parse(dateString);
+        } catch (Exception e) {
+            date = null;
+        }
         return date;
     }
-    
+
     /**
-     * 
-     *description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
-     *@author rongnian.lu gfwu321@163.com
-     *date： 2015-7-28
-     *param:@param dateString
-     *param:@return Date yyyy-MM-dd 
-     *return：Date
+     * description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
+     *
+     * @author rongnian.lu gfwu321@163.com
+     * date： 2015-7-28
+     * param:@param dateString
+     * param:@return Date yyyy-MM-dd
+     * return：Date
      */
     public static final java.sql.Date getSqlDateFromString(String dateString) {
-    	
+
         return new java.sql.Date(getDateFromString(dateString).getTime());
     }
-    
-    
+
+
     /**
-     * 
-     *description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
-     *@author rongnian.lu gfwu321@163.com
-     *date： 2015-7-28
-     *param:@param dateString
-     *param:@return Timestamp yyyy-MM-dd hh:mm:ss
-     *return：Date
+     * description: yyyy-MM-dd 或者 yyyy-MM-dd hh:mm:ss
+     *
+     * @author rongnian.lu gfwu321@163.com
+     * date： 2015-7-28
+     * param:@param dateString
+     * param:@return Timestamp yyyy-MM-dd hh:mm:ss
+     * return：Date
      */
     public static final java.sql.Timestamp getSqlTimestampFromString(String dateString) {
         return new java.sql.Timestamp(getDateFromString(dateString).getTime());
     }
+
     private static final DateFormat getFormat(String format) {
         return new SimpleDateFormat(format);
     }
@@ -228,26 +232,27 @@ public class DateUtil {
         Date endDay = parse(dtFormat(new Date(endTime)), dSimple);
         return (int) Math.round((endDay.getTime() - startDay.getTime()) / (3600000 * 24d));
     }
-    
+
     /**********************数字转换，暂时先放这个类**********************/
-    private static final String[] UNITS = { "", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百",
-            "千"                        };
+    private static final String[] UNITS = {"", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百",
+            "千"};
 
-    private static final String[] NUMS  = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+    private static final String[] NUMS = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"};
 
-    /** 
-     *  数字转换成中文汉字 
-     *  @param value  转换的数字 
-     *  @return 返回数字转后的汉字字符串 
+    /**
+     * 数字转换成中文汉字
+     *
+     * @param value 转换的数字
+     * @return 返回数字转后的汉字字符串
      */
     public static String translate(int value) {
         if (value <= 0) {
             return "未出生";
         }
         String result = "";
-        int year = value/12;
-        int month =value%12;
-        if(year>0){
+        int year = value / 12;
+        int month = value % 12;
+        if (year > 0) {
             result = getHanValue(year) + "岁";
         }
         if (month > 0) {
@@ -273,19 +278,20 @@ public class DateUtil {
         if (result.endsWith("零"))
             result = result.substring(0, result.length() - 1);
         return result;
- 
+
     }
-    
+
     /**
      * 把yyyyMMdd格式日期字符串格式化成YYYY-MM-DD格式,把kkmmss格式时间字符串格式化成kk:mm:ss格式,中间加空格后拼接
+     *
      * @param date yyyyMMdd格式日期字符串
      * @param time kkmmss格式时间字符串
      * @return
      */
     public static String formatDateTimeFromDB(String date, String time) {
-        if( StringUtils.isEmpty(date) || date.length() < 8 )
+        if (StringUtils.isEmpty(date) || date.length() < 8)
             date = "        ";
-        if( StringUtils.isEmpty(time) || time.length() < 6 )
+        if (StringUtils.isEmpty(time) || time.length() < 6)
             time = "      ";
         StringBuilder buf = new StringBuilder(date);
         buf.insert(6, '-').insert(4, '-');
@@ -293,21 +299,64 @@ public class DateUtil {
         buf1.insert(2, ':').insert(5, ':');
         return buf.toString() + " " + buf1.toString();
     }
+
     /**
      * yyyyMMdd HHmmss 转换成 yyyy-MM-dd HH:mm:ss
+     *
      * @param createDate yyyyMMdd
      * @param createTime HHmmss
      * @return
      */
-    public static String formatFromDB (String createDate, String createTime) {
+    public static String formatFromDB(String createDate, String createTime) {
 
-    	String dateStr = "";
-		if (!StringUtils.isEmpty(createDate) && !StringUtils.isEmpty(createTime)) {
-	    	Date date = DateUtil.parse(createDate + " " + createTime, "yyyyMMdd HHmmss");
-	    	dateStr = DateUtil.dtFormat(date, DateUtil.dttmSimple);
-	}
-		return dateStr;
-}
-    
-    
+        String dateStr = "";
+        if (!StringUtils.isEmpty(createDate) && !StringUtils.isEmpty(createTime)) {
+            Date date = DateUtil.parse(createDate + " " + createTime, "yyyyMMdd HHmmss");
+            dateStr = DateUtil.dtFormat(date, DateUtil.dttmSimple);
+        }
+        return dateStr;
+    }
+
+    /**
+     * 得到输入日期+i天以后的日期
+     * @param s yyyyMMdd格式日期
+     * @param i 可以是负数
+     * @return
+     */
+    public static String getNextDateByNum(String s, int i) {
+        SimpleDateFormat simpledateformat = new SimpleDateFormat("yyyyMMdd");
+        java.util.Date date = simpledateformat.parse(s, new ParsePosition(0));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, i);
+        date = calendar.getTime();
+        s = simpledateformat.format(date);
+        return s;
+    }
+
+    /**
+     * 获取某个日期所在周的周一的日期
+     * @param dataStr
+     * @return
+     */
+    public static String getFirstOfWeek(String dataStr) {
+        String firstDate = "";
+        try {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(new SimpleDateFormat("yyyyMMdd").parse(dataStr));
+            int d;
+            if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+                d = -6;
+            } else {
+                d = 2 - cal.get(Calendar.DAY_OF_WEEK);
+            }
+            cal.add(Calendar.DAY_OF_WEEK, d);
+            // 所在周开始日期
+            firstDate = new SimpleDateFormat("yyyyMMdd").format(cal.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return firstDate;
+    }
+
 }
