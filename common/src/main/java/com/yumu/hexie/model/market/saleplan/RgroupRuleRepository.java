2da196ed5bbb1d8f2bea38eb3a050fff5db9f7dd
davidhardson <davidhardson@hotmail.com>
2022-03-27 13:55:44 +0800

    Devhuym41 (#265)
    * 团购

--- common/src/main/java/com/yumu/hexie/model/market/saleplan/RgroupRuleRepository.java
+++ common/src/main/java/com/yumu/hexie/model/market/saleplan/RgroupRuleRepository.java
@@ -57,10 +57,11 @@ public interface RgroupRuleRepository extends JpaRepository<RgroupRule, Long> {
 			+ "and (COALESCE(?4) IS NULL OR (rule.groupStatus IN (?4) )) "
 			+ "and IF (?5!='', rule.createDate >= ?5, 1=1) "
 			+ "and IF (?6!='', rule.createDate <= ?6, 1=1) "
-			+ "and IF (?7!='', a.id = ?7, 1=1) "
+			+ "and IF (?7!='', a.agentNo = ?7, 1=1) "
 			+ "and IF (?8!='', p.demo = ?8, 1=1) "
 			+ "and IF (?9!='', item.areaLeaderId = ?9, 1=1) "
 			+ "and (COALESCE(?10) IS NULL OR (item.regionId IN (?10) )) "
+			+ "and rule.endDate <= ?11 "
 			+ "group by rule.id "
 			, countQuery = "select rule.id, count(1) from rgrouprule rule "
 					+ "join product p on rule.productId = p.id "
@@ -73,13 +74,14 @@ public interface RgroupRuleRepository extends JpaRepository<RgroupRule, Long> {
 					+ "and (COALESCE(?4) IS NULL OR (rule.groupStatus IN (?4) )) "
 					+ "and IF (?5!='', rule.createDate >= ?5, 1=1) "
 					+ "and IF (?6!='', rule.createDate <= ?6, 1=1) "
-					+ "and IF (?7!='', a.id = ?7, 1=1) "
+					+ "and IF (?7!='', a.agentNo = ?7, 1=1) "
 					+ "and IF (?8!='', p.demo = ?8, 1=1) "
 					+ "and IF (?9!='', item.areaLeaderId = ?9, 1=1) "
 					+ "and (COALESCE(?10) IS NULL OR (item.regionId IN (?10) )) "
+					+ "and rule.endDate <= ?11 "
 					+ "group by rule.id "
 					, nativeQuery = true)
 	Page<Object[]> findByMultiCondRgroup(String productType, String ruleId, String ruleName, List<Integer>groupStatus, 
-			String startDate, String endDate, String agentId, String isDemo, String leaderId, List<String>sectList, Pageable pageable);
+			String startDate, String endDate, String agentId, String isDemo, String leaderId, List<String>sectList, Date offDate, Pageable pageable);
 
 }
