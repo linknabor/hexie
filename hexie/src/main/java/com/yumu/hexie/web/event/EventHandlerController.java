b4bd9166ed456ecdea2df5da718767f3a1f1210a
davidhardson <davidhardson@hotmail.com>
2023-01-25 11:17:35 +0800

    fix yanji response

--- hexie/src/main/java/com/yumu/hexie/web/event/EventHandlerController.java
+++ hexie/src/main/java/com/yumu/hexie/web/event/EventHandlerController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.yumu.hexie.common.Constants;
+import com.yumu.hexie.common.util.JacksonJsonUtil;
 import com.yumu.hexie.integration.wechat.constant.ConstantWeChat;
 import com.yumu.hexie.model.ModelConstant;
 import com.yumu.hexie.web.BaseController;
@@ -30,10 +32,10 @@ public class EventHandlerController extends BaseController{
 	
 	
 	@RequestMapping(value = "/subscribe", method = RequestMethod.POST)
-	public String subscribe(@RequestBody String requestJson) {
+	public String subscribe(@RequestBody String requestJson) throws JsonProcessingException {
 		log.info("subscribe event : " + requestJson);
 		stringRedisTemplate.opsForList().rightPush(ModelConstant.KEY_EVENT_SCAN_SUBSCRIBE_QUEUE, requestJson);
-		return Constants.SERVICE_SUCCESS;
+		return JacksonJsonUtil.getMapperInstance(false).writeValueAsString(Constants.SERVICE_SUCCESS);
 	}
 	
 	@RequestMapping(value = "/updateToken", method = RequestMethod.POST)
