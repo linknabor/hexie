38640f2d0dd8bdd97456bca45c25098a0883172a
davidhardson <davidhardson@hotmail.com>
2023-12-12 19:31:12 +0800

    调整ip源地址

--- common/src/main/java/com/yumu/hexie/common/util/RequestUtil.java
+++ common/src/main/java/com/yumu/hexie/common/util/RequestUtil.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 public class RequestUtil {
 	
@@ -12,18 +11,30 @@ public class RequestUtil {
 	
 	public static String getRealIp(HttpServletRequest request) {
 		
-		String ipAddress = "";
-		String xForwardedFor = request.getHeader("X-Forwarded-For");
-		logger.info("X-Forwarded-For : " + xForwardedFor);
-		if (!StringUtils.isEmpty(xForwardedFor)) {
-			String[]ipArr = xForwardedFor.split(",");
-			ipAddress = ipArr[ipArr.length-1];
-		}
-		if (StringUtils.isEmpty(ipAddress)) {
-			ipAddress = request.getRemoteAddr();
-			logger.info("remote addr : " + ipAddress);
-		}
-		return ipAddress;
+		String ip = request.getHeader("x-forwarded-for");
+		logger.info("x-forwarded-for : " + ip);
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("Proxy-Client-IP");  
+            logger.info("Proxy-Client-IP : " + ip);
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("WL-Proxy-Client-IP"); 
+            logger.info("WL-Proxy-Client-IP : " + ip);
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_CLIENT_IP");  
+            logger.info("HTTP_CLIENT_IP : " + ip);
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+            logger.info("HTTP_X_FORWARDED_FOR : " + ip);
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getRemoteAddr();
+            logger.info("remoteAddr : " + ip);
+        }  
+        return ip;
+		
 	}
 
 }
