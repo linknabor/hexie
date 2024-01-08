9c77aefc88b85cd628151414c7ef266e6de4a08e
davidhardson <davidhardson@hotmail.com>
2024-01-08 14:44:40 +0800

    add debug log

--- common/src/main/java/com/yumu/hexie/service/common/impl/SystemConfigServiceImpl.java
+++ common/src/main/java/com/yumu/hexie/service/common/impl/SystemConfigServiceImpl.java
@@ -171,11 +171,16 @@ public class SystemConfigServiceImpl implements SystemConfigService {
                    log.error("queryWXAccToken failed :", e);
                 }
             }
-            throw new BizValidateException("微信token没有记录");
+            throw new BizValidateException("未获取到token, appid : " + appId);
 		
     	} else {
     		
-    		String authorizerAccessToken = redisRepository.getAuthorizerAccessToken(ConstantWeChat.KEY_AUTHORIZER_ACCESS_TOKEN + appId);
+    		String authorizerAccessToken = "";
+			try {
+				authorizerAccessToken = redisRepository.getAuthorizerAccessToken(ConstantWeChat.KEY_AUTHORIZER_ACCESS_TOKEN + appId);
+			} catch (Exception e) {
+				log.error("queryWXAccToken failed :", e);
+			}
     		return authorizerAccessToken;
     		
 		}
