99fef0d189057bd1e09a67540bbec7b6888661d4
davidhardson <davidhardson@hotmail.com>
2024-01-18 13:11:22 +0800

    add log 4 debug

--- hexie/src/main/java/com/yumu/hexie/web/shequ/CommunityController.java
+++ hexie/src/main/java/com/yumu/hexie/web/shequ/CommunityController.java
@@ -86,6 +86,10 @@ public class CommunityController extends BaseController{
 	@ResponseBody
 	public BaseResult<String> addThread(@ModelAttribute(Constants.USER)User user, @RequestBody SaveInteractInfoReq req) throws Exception{
 		log.info("content length : " + req.getEx_content().length());
+		log.info("content length 2 : " + StringUtils.length(req.getEx_content()));
+		String content = req.getEx_content();
+		log.info("content length 3 : " + content.length());
+		
 		if(req.getEx_content().length()>200) {
 			return BaseResult.fail("发布信息内容超过200字。");
 		}
