f9c7bfed9a08bd99b2ada481154e40c363019acb
davidhardson <davidhardson@hotmail.com>
2024-01-18 13:26:20 +0800

    fix投诉建议汉字长度翻倍的BUG

--- hexie/src/main/java/com/yumu/hexie/web/shequ/CommunityController.java
+++ hexie/src/main/java/com/yumu/hexie/web/shequ/CommunityController.java
@@ -82,13 +82,9 @@ public class CommunityController extends BaseController{
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/interact/addInteract", method = RequestMethod.POST)
+	@RequestMapping(value = "/interact/addInteract", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public BaseResult<String> addThread(@ModelAttribute(Constants.USER)User user, @RequestBody SaveInteractInfoReq req) throws Exception{
-		log.info("content length : " + req.getEx_content().length());
-		log.info("content length 2 : " + StringUtils.length(req.getEx_content()));
-		String content = req.getEx_content();
-		log.info("content length 3 : " + content.length());
 		
 		if(req.getEx_content().length()>200) {
 			return BaseResult.fail("发布信息内容超过200字。");
