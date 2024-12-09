72e22f480f60b6a31356e34c9d11527b6ffc0397
davidhardson <davidhardson@hotmail.com>
2024-12-09 17:55:07 +0800

    支付宝订阅消息

--- common/src/main/java/com/yumu/hexie/service/subscribemsg/impl/AliSubscribeMsgServiceImpl.java
+++ common/src/main/java/com/yumu/hexie/service/subscribemsg/impl/AliSubscribeMsgServiceImpl.java
@@ -1,7 +1,11 @@
 package com.yumu.hexie.service.subscribemsg.impl;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.Resource;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +20,24 @@ public class AliSubscribeMsgServiceImpl implements AliSubscribeMsgService {
 
 	@Resource
 	private UserSubscribeMsgRepository userSubscribeMsgRepository;
+	@Resource
+	private StringRedisTemplate stringRedisTemplate;
 	
 	@Transactional
 	@Override
 	public void addSubscribe(SubscribeReq subscribeReq) {
 		
+		String lockKey = "lock:alimsg:subscribe:";
 		User aliUser = subscribeReq.getUser();
+		lockKey += (aliUser.getAliappid() + ":" + aliUser.getAliuserid());
+		Boolean exists = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "1", 1800l, TimeUnit.SECONDS);
+		if (!exists) {
+			return;
+		}
+		List<UserSubscribeMsg> subscribeList =userSubscribeMsgRepository.findByAliuseridAndAppid(aliUser.getAliuserid(), aliUser.getAliappid());
+		if(subscribeList!= null && !subscribeList.isEmpty()) {
+			return;
+		}
 		UserSubscribeMsg userSubscribeMsg = new UserSubscribeMsg();
 		userSubscribeMsg.setAliuserid(aliUser.getAliuserid());
 		userSubscribeMsg.setAppid(aliUser.getAliappid());
