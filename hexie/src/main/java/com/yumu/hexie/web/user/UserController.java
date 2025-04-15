fbfa3fa252b34ee3240377b675dce9aacfa0a185
liuzj_jj@163.com <liuzj_jj@163.com>
2025-04-15 11:43:40 +0800

    切换小区，重新获取小区参数

--- hexie/src/main/java/com/yumu/hexie/web/user/UserController.java
+++ hexie/src/main/java/com/yumu/hexie/web/user/UserController.java
@@ -747,6 +747,9 @@ public class UserController extends BaseController{
 	        List<Menu> menuList = pageConfigService.getMenuByAppidAndDefaultTypeLessThan(user.getMiniAppId(), 2);	//表示绑定了房屋的默认菜单
 		    userInfo.setMenuList(menuList);
 		    userInfo.setPermission(true);
+
+			Map<String, String> paramMap = paramService.getWuyeParam(user);
+			userInfo.setCfgParam(paramMap);
 		}
 	   
 	    		    
